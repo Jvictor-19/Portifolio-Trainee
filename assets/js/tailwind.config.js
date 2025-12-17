c6f847046b41bdb25ce03fbfde818cdff3f9c89b
Jvictor-19 <164103348+Jvictor-19@users.noreply.github.com>
2025-12-16 21:56:28 -0300

    refactor: criação e ajuste da main e tailwind na pasta js

---
+++ assets/js/tailwind.config.js
@@ -0,0 +1,34 @@
+// assets/js/tailwind.config.js
+tailwind.config = {
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: [
+          "ui-sans-serif",
+          "system-ui",
+          "-apple-system",
+          "BlinkMacSystemFont",
+          "Segoe UI",
+          "Roboto",
+          "Helvetica Neue",
+          "Arial",
+          "sans-serif",
+        ],
+        mono: [
+          "ui-monospace",
+          "SFMono-Regular",
+          "Menlo",
+          "Monaco",
+          "Consolas",
+          "Liberation Mono",
+          "Courier New",
+          "monospace",
+        ],
+      },
+      animation: {
+        "spin-slow": "spin 12s linear infinite",
+        "pulse-slow": "pulse 4s cubic-bezier(0.4, 0, 0.6, 1) infinite",
+      },
+    },
+  },
+};
